b2deba45f6f4a4b36b46bb83215e200d5292e123
NgocToan-dev <pntoan156@gmail.com>
2025-06-25 21:31:50 +0700

    Reinit project

--- src/plugins/i18n/locales/en/notFound.ts
+++
@@ -1,6 +0,0 @@
-export const notFound = {
-  title: 'Page Not Found',
-  description: "The page you're looking for doesn't exist or has been moved.",
-  goHome: 'Go Home',
-  goBack: 'Go Back',
-}
--- src/plugins/i18n/locales/vi/notFound.ts
+++
@@ -1,6 +0,0 @@
-export const notFound = {
-  title: 'Không tìm thấy trang',
-  description: 'Trang bạn đang tìm kiếm không tồn tại hoặc đã bị di chuyển.',
-  goHome: 'Về trang chủ',
-  goBack: 'Quay lại',
-}
